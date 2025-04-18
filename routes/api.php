28e3eca88ad0074d6070ca69c8288fdba34c8b19
shoaib <shoaib66885@gmail.com>
2025-04-18 11:17:06 +0500

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+use App\Http\Controllers\Api\UserController;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+ApiRoute::group(['namespace' => 'App\Http\Controllers'], function () {
+    ApiRoute::get('purchased-module', ['as' => 'api.purchasedModule', 'uses' => 'HomeController@installedModule']);
+});
+
+Route::get('/users', [UserController::class, 'index']);
